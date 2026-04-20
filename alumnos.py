cbb574322c53f8293f7e162d49284c0a3348ddbe
Adan Sanchez <asanchez00@unisalle.edu.co>
2026-04-20 15:43:13 -0500

    Creación de clases base

---
+++ alumnos.py
@@ -0,0 +1,4 @@
+class alumno:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
---
+++ colegio.py
@@ -0,0 +1,13 @@
+class colegio:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.alumnos = []
+
+    def agregar_alumno(self, alumno):
+        self.alumnos.append(alumno)
+
+    def mostrar_alumnos(self):
+        print(f"Alumnos en el colegio {self.nombre}:")
+        for alumno in self.alumnos:
+            print(f"- {alumno.nombre}, Edad: {alumno.edad}")
+            
